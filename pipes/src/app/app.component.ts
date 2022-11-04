e2340792629e58672e52121046135e13d9cd4fa6
liana13555 <barishovaliana@gmail.com>
2022-11-04 14:17:57 +0100

    Updated template, added pipes

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -8,9 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name: string = ''
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height!: number;  //for decimal pipe - десятичный
+  miles!: number;  // creating custom pipe
+
+  car = {                     // for json pipe
+    make: 'Toyota',  
+    model: 'Camry',
+    year: 2000,
+  }
 
   onNameChange(value: string) {
     this.name = value
+  }
+
+  onDateChange(value: string) {
+    this.date = value
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value)
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value)
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value)
   }
 }
---
+++ pipes/src/app/convert.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert'
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: any, targetUnits: string): any {
+    if (!value) {
+      return ''
+    }    
+  // console.log(targetUnits);
+    
+  switch (targetUnits) {
+    case 'km':
+      return value * 1.60934
+    case 'm':
+      return value * 1.60934 * 1000
+    case 'cm':
+      return value * 1.60934 * 1000 * 100
+  
+    default:
+      throw new Error('Target unit not supported')
+  }   
+    return value * 1.60934
+  }
+
+}
